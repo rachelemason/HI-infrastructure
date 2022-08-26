be03fa8cd6e3cefef2039611dd71fb4cfae303a0
rachelemason <rachel.e.mason1@gmail.com>
2022-08-25 18:20:15 -0700

    Fixed indexing error in cnn_support.Loops.chunks; small improvements to plots and readability elsewhere in cnn_support.py

--- cnn_support/cnn_support.py
+++ cnn_support/cnn_support.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #cnn_support.py
-#REM 2022-08-18
+#REM 2022-08-25
 
 """
 Code to support use of the BFGN package (github.com/pgbrodrick/bfg-nets),
@@ -310,7 +310,7 @@ class AppliedModel():
 
 
     @classmethod
-    def printstats(cls, statsdict):
+    def print_stats(cls, statsdict):
         """
         Display the dictionary of performance stats returned by self.performance_metrics()
         as a pandas dataframe. INCOMPLETE
@@ -389,23 +389,23 @@ class Loops(Utils, AppliedModel):
         shutil.rmtree(model_out, ignore_errors=True)
 
 
-    def _run_loops(self, i, size, combo):
+    def _run_loops(self, i, size, batchnum, rebuild):
         """
         Helper function for self.chunks(). Creates a batch of parameters and
         calls loop_over_configs to run the CNN and get the performance stats.
         """
 
         batch = self.parameter_combos[i:i+size]
-        stats = self.loop_over_configs(batch)
+        stats = self.loop_over_configs(batch, i, rebuild)
 
-        with open(f'{self.out_path}combo_{combo}.json', 'w', encoding='utf-8') as f:
+        with open(f'{self.out_path}batch_{batchnum}.json', 'w', encoding='utf-8') as f:
             json.dump(stats, f)
-            print(f'Saved results for batch {combo} to {self.out_path}combo_{combo}.json')
+            print(f'Saved results for batch {batchnum} to {self.out_path}batch_{batchnum}.json')
 
         return stats
 
 
-    def chunks(self, size=4, use_existing=True):
+    def chunks(self, size=4, use_existing=True, rebuild=True):
         """
         Divide the tuple containing parameter combinations (self.parameter_combos)
         into pieces of size=4 parameter combos, feed them into loop_over_configs,
@@ -420,43 +420,48 @@ class Loops(Utils, AppliedModel):
         if self.parameter_combos changes)
         """
 
+        assert len(self.parameter_combos) == len(self.nicknames),\
+                          'Number of nicknames should equal the number of parameter combinations'
+
         #Array to hold the performance metrics
         self.stats_array = np.zeros((len(self.parameter_combos),\
-                                     len(self.app_features)*3))
+                                     len(self.app_features)*3)) #3 is for the 3 metrics
 
-        combo = 0
+        batchnum = 0
+        j = 0
         for i in range (0, len(self.parameter_combos), size):
+            print(f'**Starting batch {batchnum} of {int(np.ceil(len(self.parameter_combos)/size))}')
 
             if use_existing:
                 try:
-                    with open(f'{self.out_path}combo_{combo}.json', 'r', encoding='utf-8') as f:
+                    with open(f'{self.out_path}batch_{batchnum}.json', 'r', encoding='utf-8') as f:
                         stats = json.load(f)
-                        print(f'Loading stats for batch {combo} from file')
+                        print(f'Loading stats for batch {batchnum} from file')
                 except FileNotFoundError:
-                    print(f'Saved stats not found for batch {combo}; running model')
-                    stats = self._run_loops(i, size, combo)
+                    print(f'Saved stats not found for batch {batchnum}; running model')
+                    stats = self._run_loops(i, size, batchnum, rebuild)
 
             else:
-                stats = self._run_loops(i, size, combo)
+                stats = self._run_loops(i, size, batchnum, rebuild)
 
             #Indexing here gets complicated. Variable <stats> contains len(self.app_features)
             #items per parameter combo, and len(batch) items for each of those. We want to
             #deconstruct that into an ndarray with one row for each parameter combo, and
             #(precision, recall, and f1-score) for each of self.app_features in the columns
-            step = int(len(stats) / len(self.app_features))
-            n = 0
             for i in range (0, len(stats), len(self.app_features)):
-                #stats for class 1 (building)
+                #stats for one parameter combo, for len(self.app_features) test fields
+                print(f'Stats for combo {j} going into row {j} of stats_array...')
                 statsbatch = stats[i:i+len(self.app_features)]
                 for idx, data in enumerate(statsbatch):
-                    self.stats_array[combo*step+n, idx*3] = np.round(data['1.0']['precision'], 2)
-                    self.stats_array[combo*step+n, idx*3+1] = np.round(data['1.0']['recall'], 2)
-                    self.stats_array[combo*step+n, idx*3+2] = np.round(data['1.0']['f1-score'], 2)
-                n += 1
-            combo += 1
+                    #stats for class '1.0' (buildings)
+                    self.stats_array[j, idx*3] = np.round(data['1.0']['precision'], 2)
+                    self.stats_array[j, idx*3+1] = np.round(data['1.0']['recall'], 2)
+                    self.stats_array[j, idx*3+2] = np.round(data['1.0']['f1-score'], 2)
+                j += 1
+            batchnum += 1
 
 
-    def loop_over_configs(self, parameter_combos):
+    def loop_over_configs(self, parameter_combos, start_idx, rebuild=True):
         """
         Loops over BFGN configurations (can be training data or other parameters in the
         config file), and returns a numpy array of model performance metrics (precision,
@@ -470,6 +475,7 @@ class Loops(Utils, AppliedModel):
 
         #Loop over the parameter combos, fit model  record metrics
         for idx, params in enumerate(parameter_combos):
+            num = start_idx + idx
 
             #get the current set of parameters
             combo_dict = {}
@@ -478,7 +484,7 @@ class Loops(Utils, AppliedModel):
                 combo_dict[name] = j
 
             print('\n===================================================')
-            print(f'Testing parameter combination #{idx}:\n')
+            print(f'Testing parameter combination #{num}:\n')
             for key, value in combo_dict.items():
                 print(f'{key}: {value}')
 
@@ -506,10 +512,10 @@ class Loops(Utils, AppliedModel):
                         newlines.append(line)
 
             #create a new settings file and output dir for this parameter combo
-            with open(f'new_settings_{idx}.yaml', 'w', encoding='utf-8') as f:
+            with open(f'new_settings_{num}.yaml', 'w', encoding='utf-8') as f:
                 for line in newlines:
                     f.write(line)
-            os.makedirs(f'{self.out_path}combo_{idx}', exist_ok=True)
+            os.makedirs(f'{self.out_path}combo_{num}', exist_ok=True)
 
             #delete existing munged data and model output to avoid errors
             shutil.rmtree('./data_out', ignore_errors=True)
@@ -517,9 +523,9 @@ class Loops(Utils, AppliedModel):
 
             #fit the model using this parameter combo, suppressing the very verbose output
             with io.capture_output():
-                config = configs.create_config_from_file(f'new_settings_{idx}.yaml')
+                config = configs.create_config_from_file(f'new_settings_{num}.yaml')
                 data_container = data_core.DataContainer(config)
-                data_container.build_or_load_rawfile_data(rebuild=True)
+                data_container.build_or_load_rawfile_data(rebuild=rebuild)
                 data_container.build_or_load_scalers()
                 data_container.load_sequences()
                 experiment = experiments.Experiment(config)
@@ -537,22 +543,21 @@ class Loops(Utils, AppliedModel):
                     applied_model = self.tif_to_array(self.default_out+self.app_outnames[i]+'.tif')
                     #archive the applied model tif
                     shutil.move(f'{self.default_out+self.app_outnames[i]}.tif',\
-                                f"{self.out_path}combo_{idx}/{self.app_outnames[i]}.tif")
+                                f"{self.out_path}combo_{num}/{self.app_outnames[i]}.tif")
                     #make pdf showing applied model
                     self.show_applied_model(applied_model, zoom=self.zooms[i], original_img=_f[0],\
-                                       responses=self.app_responses[i], filename=\
-                                       f"{self.out_path}combo_{idx}/{self.app_outnames[i]}\
-                                       .pdf")
+                                        responses=self.app_responses[i], filename=\
+                                        f"{self.out_path}combo_{num}/{self.app_outnames[i]}.pdf")
                     #get performance metrics
                     stats.append(self.performance_metrics(applied_model, self.app_responses[i],\
-                                                self.app_boundaries[i]))
+                                 self.app_boundaries[i]))
 
             #TODO: record/return number of training samples for future use/plots
             n_samples = np.sum([len(n) for n in data_container.features])
             print(f'{n_samples} training samples were extracted from the data\n')
 
             #archive model report and config file for this parameter combo/setting
-            self._archive_and_tidy(idx, './data_out', self.default_out)
+            self._archive_and_tidy(num, './data_out', self.default_out)
 
             #close all figures in the hope of not creating memory problems
             plt.close('all')
@@ -568,7 +573,7 @@ class Loops(Utils, AppliedModel):
 
         print(f'Parameters tested:{list(self.permutations.keys())}')
 
-        _, ax = plt.subplots(figsize=(10, 10))
+        _, ax = plt.subplots(figsize=(20, 20))
         img = ax.imshow(self.stats_array, vmin=0, vmax=1, cmap='hot')
         _ = plt.colorbar(img, shrink=0.55)
 
@@ -612,9 +617,9 @@ class Loops(Utils, AppliedModel):
         data set; one subplot for each of the files to which the model output was applied.
         """
 
-        fig, _ = plt.subplots(1, 3, figsize=(12, 4))
+        fig, _ = plt.subplots(1, len(self.app_outnames), figsize=(16, 4))
 
-        for (j, _), ax in zip(enumerate(self.nicknames), fig.axes):
+        for (j, _), ax in zip(enumerate(self.app_outnames), fig.axes):
             precision = []
             recall =[]
             f_1 = []
